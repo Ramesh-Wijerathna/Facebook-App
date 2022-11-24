7f0747898d513ac1dfdc8b9fb117105f0ad70b9c
Ramesh Wijerathna <>
2022-11-24 17:14:22 +0530

    Added View Model and Changed background color

---
+++ My Facebook/My Facebook/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  My Facebook
+//	
+//  Created by Ramesh Wijerathna on 2022-11-24 at 5:13 PM.
+//  Copyright © 2022 Ramesh Wijerathna. All rights reserved.
+//  Swift version:  5.0
+//
+
+
+import Foundation
+
+
+class ViewModel {
+    
+    
+    
+}

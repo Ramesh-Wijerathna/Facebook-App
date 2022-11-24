21ed993a3e8afbeb032648a6ffde8a9ebfb2f545
Ramesh Wijerathna <>
2022-11-24 17:12:33 +0530

    System files

---
+++ My Facebook/My Facebook/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  My Facebook
+//	
+//  Created by Ramesh Wijerathna on 2022-11-24 at 5:11 PM.
+//  Copyright © 2022 Ramesh Wijerathna. All rights reserved.
+//  Swift version:  5.0
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
